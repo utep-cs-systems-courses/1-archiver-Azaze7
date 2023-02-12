b9195ad29e1999bce2eb86ee7638a9453f494176
Azaze7 <97211914+Azaze7@users.noreply.github.com>
2023-02-12 13:57:14 -0700

    Add files via upload
    +Added More Personal Documentation, project outlines, and UNIX shebang; alongside current plan for main function.
    --------------------------------------------------------------------------------------------------------------------------
    -Removed Example code given by professor, as I can now run those examples independently on their own.
    -Removed current unfinished code inside mytar until it is closer to completion, it has an embarrassing amount of compilation errors currently.

--- mytar.py
+++ mytar.py
@@ -1,11 +1,27 @@
-#---------------------------------------------------------------------------------
+#!/usr/bin/env python
+
+#***The Above line must be included for UNIX, it is a she-bang.
+#--------------------------------------------------------------------------------------------
 # Gilbert I. Guzman -- 88769422
 # CS 4375 -- CRN: 24866 -- Dr. Fruedenthal
-#---------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
 # Lab 1 - Student Code Submission -- 'mytar.py'
-# Due: 2/13/23 -- Present to TA
-#---------------------------------------------------------------------------------
-# Assignment: Build a toy version of tar. (Tape Archiver)
+# Due: 2/13/23 -- Present to TA -- [2:10-2:30]
+#--------------------------------------------------------------------------------------------
+# LEARNING OUTCOMES REFLECTION:
+    #1. File Descriptors. [NO]
+        #Can read and write from stdin, stdout, stderr;
+            #and named files using system calls.
+    #2. Interpreted Programs. [NO]
+        #Made code that works directly from the command line.
+        #Knows how to grant permission to execute, use shebang, 
+            #and pass parameters using argv.
+    #3. Framing. [NO]
+        #Made code that embed and extract multiple byte arrays, which includes files
+            #that contain headers that are used to decribe the file that are NOT
+            #the file itself.
+#--------------------------------------------------------------------------------------------
+# Assignment Instructions: Build a toy version of tar. (Tape Archiver)
     #2 MAJOR FUNCTIONS:
         #1.(c) Create
             #Creates a new archive on the 'tape' we are working with.
@@ -23,40 +39,67 @@
         #WE MUST CHUNK FILES IN BYTES SO WE CAN SEND IT FAST.
         #IT MUST BE ABLE TO ERROR REPORT, THAT BEING, RETURN A 2 = ERROR;
             #INSTEAD OF JUST 0 = IN AND 1 = OUT. A MESSAGE SHOULD BE PRINTED TOO!
-#---------------------------------------------------------------------------------
+#--------------------------------------------------------------------------------------------
+# BRAINSTORMING (REAL WORK):
+# RESEARCH USING UTF-8 ENCODING SINCE THAT IS BYTES AUTOMATICALLY.
+    #Main Function:
+        #Must be able to take the system arguments the user inputs into it, 
+        #[mytar] [c or x] [file(s) or folder] [< or >] [new.tar or extracted.tar]
+    #***WE WILL DO THIS USING IF/ELSE FORMING A MENU SYSTEM.***
+        #Logic:
+            #If the system args =< 3: (3 since thats in example; tar x < /dev/mt0)
+            #Print message on how its supposed to work, then kill the program.
+        #Else:
+            #if operand == [c]:
+                #Call create_function.
+            #elif operand == [x]:
+                #Call extract_function.
+            #else
+                #Print out message about not using a real operation, kill program.
+    
+    #Create_function:
+    #DOESN'T Actually create anything, just reads the source file and writes to stdout.
 
-#Default Imports. (WE ARE ONLY ALLOWED THESE 2).
+    #Extract_function:
+
+
+#--------------------------------------------------------------------------------------------
+# STUDENT RESEARCH:
+    #Tar (.tar) stands for Tape Archive Files.
+    #It keeps many files in a single file, similar to a .zip.
+    #You can take stuff out of it, or put stuff in using directors (< or >).
+#--------------------------------------------------------------------------------------------
+# USING UBUNTU-LINUX:
+    #***You Can Drag And Drop files From Windows ---> UBUNTU!!!! [DR. FRUEDENTHAL TIP]***
+    #1. Start Ubuntu using the installed app.
+    #2. Find the file.
+        #cd /tmp/
+        #Opens tmp directory, where I will be putting code for a while.
+    #3. Change File Permissions.
+        #chmod +(x or r or w) <file>.py
+            #x is for execute.
+            #r is for run.
+            #w is for write.
+    #4. Run the file.
+        #$_ python3 mytar.py
+        #Will run the file now, compilation errors because I dont know how linux works.
+#--------------------------------------------------------------------------------------------
+
+#Default Imports. (WE ARE ONLY ALLOWED THESE 2 TO MY KNOWLEGE).
     #[from os import read, write]
 import os
 import sys
+
+#Print Default Code Messages.
+print("------")
 
 #Name of file input.
 progName = sys.argv[0]
 #Name of file output.
 oFileName = sys.argv[1]      
 
-#CAT COMPONENETS (beta, stolen from example code via Dr. Fruedenthal)
-#Demo of os file open/read/close semantics.
 
-def printFromFd(ifd):
-    numReads = 1
-    while True:
-        ibuf = os.read(ifd, 100)
-        if not len(ibuf): break
-        numReads += 1
-        os.write(1, ibuf)
-     # summary to stderr
-    os.close(ifd)
-    os.write(2, f"EOF on {numReads}th call to read()\n".encode())
-    
 
-filesToPrint = sys.argv[1:]     # all params are file names
-
-if len(filesToPrint):
-    for fname in filesToPrint:
-        os.write(2, f"copying file: {fname}\n".encode()) # to stderr
-        fd = os.open(fname, os.O_RDONLY)
-        printFromFd(fd)
-else:
-    os.write(2, "copying from stdin\n".encode()) # to stderr
-    printFromFd(0)              # stdin by default (no files)
+#--------------------------------------------------------------------------------------------
+# END OF CODE.
+ 
